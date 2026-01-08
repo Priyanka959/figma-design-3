c03581d4819c3302b1863253300b6e38d84da89e
Priyanka959 <priyankakulal1205@gmail.com>
2026-01-08 07:45:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     domains: ['images.unsplash.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+
